6e1d1cb48e205d0401c6a96d9edd7a42e53704f1
sandeep <sandeep@pepr.mobi>
2019-02-12 11:09:00 +0530

    added php form

---
+++ test_mail.php
@@ -0,0 +1,29 @@
+<?php
+if((isset($_POST['cname'])&&isset($_POST['contact'])) {
+	$name = $_POST['cname'];
+	$contact = $_POST['contact'];
+	$to = 'dsandeep35@gmail.com';
+	$subject = "new subscriber";
+	$body = '<html>
+				<body>
+					<h2> form filled  applerepairs.in  </h2>
+					<hr>
+					<p> Name <br>'.$cname.'</p>
+					<p> contact <br>'.$contact.'</p>
+				</body>
+			</html>';
+//headers
+			$headers = "Form:".$name." <".$contact.">\r\n";
+			$headers = "Reply-To: ".$contact."\r\n";
+			$headers = "MIME-Version: 1.0\r\n";
+			$headers = "Content-type: text/html; charset-utf-8";
+//send
+			$send = mail($to, $subject, $body, $headers);
+			if($send) {
+				echo "<br>";
+				echo "Thanks for contacting me. i will be with you shortly";
+			} else {
+				echo "error";
+			}
+		}
+	?>
